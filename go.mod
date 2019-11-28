04ce16a9dffef8a703b11a0454c2d792de0b6035
Nam Nguyen <namnguyen@axon.com>
2019-11-28 15:04:03 +0700

    Voila

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.13
 
 require (
 	github.com/go-sql-driver/mysql v1.4.1
+	github.com/gorilla/mux v1.7.3
+	github.com/jinzhu/gorm v1.9.11
 	google.golang.org/appengine v1.6.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,42 +1,28 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
+	"playground/handlers"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
-// User struct hehe
-type User struct {
-	Name string `json:"name"`
+func main() {
+	fmt.Println("Go ORM Tutorial")
+
+	handlers.InitialMigration()
+	handleRequests()
 }
 
-func main() {
-	fmt.Println("Go & MySQL!")
-
-	db, err := sql.Open("mysql", "root:13121992@/testdb")
-	defer db.Close()
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	results, err := db.Query("select name from users")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	for results.Next() {
-		var user User
-
-		err = results.Scan(&user.Name)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		fmt.Println(user.Name)
-	}
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", handlers.HelloWorld).Methods("GET")
+	myRouter.HandleFunc("/users", handlers.AllUsers).Methods("GET")
+	myRouter.HandleFunc("/user/{name}/{email}", handlers.AddUser).Methods("POST")
+	myRouter.HandleFunc("/user/{name}", handlers.AddUser).Methods("DELETE")
+	myRouter.HandleFunc("/user/{name}/{email}", handlers.AddUser).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
