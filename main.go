71d78334b0005be19c398366b20c5a6412b3ad02
Nam Nguyen <namnguyen@axon.com>
2020-01-02 16:09:59 +0700

    Clean up

--- main.go
+++ main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"playground/handlers"
-
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
-)
-
 func main() {
-	fmt.Println("Go ORM Tutorial")
 
-	handlers.InitialMigration()
-	handleRequests()
-}
-
-func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", handlers.HelloWorld).Methods("GET")
-	myRouter.HandleFunc("/users", handlers.AllUsers).Methods("GET")
-	myRouter.HandleFunc("/user/{name}/{email}", handlers.AddUser).Methods("POST")
-	myRouter.HandleFunc("/user/{name}", handlers.AddUser).Methods("DELETE")
-	myRouter.HandleFunc("/user/{name}/{email}", handlers.AddUser).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
